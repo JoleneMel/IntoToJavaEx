6edfd75b5c506d93fa5b70881a82514c0ebbbdba
JoleneMel <jolenemelanson33@gmail.com>
2023-01-12 14:18:10 -0500

    working

---
+++ src/week1/Variables_Java_DataTypes.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Variables_Java_DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/week2/ForLoopEx.java
@@ -0,0 +1,10 @@
+package week2;
+
+public class ForLoopEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
