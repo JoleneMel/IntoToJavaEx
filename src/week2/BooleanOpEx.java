a406e87dd1894457c107212b66dc77465d41376b
JoleneMel <jolenemelanson33@gmail.com>
2023-01-14 20:32:16 -0500

    working

--- src/week2/BooleanOpEx.java
+++ src/week2/BooleanOpEx.java
@@ -4,7 +4,25 @@ public class BooleanOpEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		Some Observations:
 
+		    The code above will compare our two variables:   ageRequiredToDrive and currentAge 
+		    We use an if statement to determine if currentAge is greater than or equal to ageRequiredToDrive
+		        If the value stored currentAge is greater than or equal to agetRequiredToDrive the result of the Boolean expression (the operation using the >= operator) based on the values assigned to those variables would evaluate to true 
+		        If the value stored in currentAge was less than ageRequiredToDrive, then the result would be false. 
+		    The result of the Boolean expression is assigned to the variable canPersonDrive and then printed to the console. In this case, the Boolean expression evaluates to false. 
+		    Note:  If currentAge were 16, 17, or another higher number, it would evaluate to true
+
+
+		List of Boolean operators that exist:
+
+		    Less than:   <
+		    Greater than:   >
+		    Less than or equal to:   <=
+		    Greater than or equal to:   >=
+		    Equal (type matters):   ==
+		    */
 	}
 
 }
